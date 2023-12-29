f4b7b5f897835fcded353fa8bc68afd289e59c84
Nikolay Dimov <dimovv.nikolay@gmail.com>
2023-12-29 16:24:29 +0200

    bug fix

--- src/api.ts
+++ src/api.ts
@@ -5,23 +5,28 @@ import { encryptQuestions } from "./crypto";
 import { decryptQuestions } from "./crypto";
 import { decryptScore } from "./crypto";
 
+type HtmlElement = HTMLElement | null;
+type ButtonElement = HTMLButtonElement | null;
+type OptionsElement = HTMLOptionElement | null;
+type InputElement = HTMLInputElement | null;
+
 const apiUrl = "https://opentdb.com/api.php";
 let selectAmount: number = 0;
 let selectDifficulty: string = "";
 let selectedCategory: string = "";
 
-const category = document.querySelector<HTMLButtonElement>("#category_span");
-const difficulty = document.querySelector<HTMLElement>("#difficulty_span");
-const question = document.querySelector<HTMLElement>("#question_span");
-const questionOptions = document.querySelector<HTMLElement>(".question-options");
-const totalQuestion = document.querySelector<HTMLElement>("#total-question");
-const selectAmountElement = document.querySelector<HTMLOptionElement>("#selected_amount");
+const category = document.getElementById("category_span") as HtmlElement;
+const difficulty = document.getElementById("difficulty_span") as HtmlElement;
+const question = document.getElementById("question_span") as HtmlElement;
+const questionOptions = document.querySelector(".question-options") as HtmlElement;
+const totalQuestion = document.getElementById("total-question") as HtmlElement;
+const selectAmountElement = document.getElementById("selected_amount") as OptionsElement;
 
-const checkBtn = document.querySelector<HTMLButtonElement>("#next-question");
-const playAgainBtn = document.querySelector<HTMLButtonElement>("#play-again");
+const checkBtn = document.getElementById("next-question") as ButtonElement;
+const playAgainBtn = document.getElementById("play-again") as ButtonElement;
 
-const result = document.querySelector<HTMLButtonElement>("#result") as HTMLElement;
-const downloadResults = document.querySelector<HTMLButtonElement>("downloadReasult");
+const result = document.getElementById("result") as HTMLElement;
+const downloadResults = document.getElementById("downloadReasult") as ButtonElement;
 
 let currentCorrectAnswer: string = "";
 let currentCorrectScore: number = 0;
@@ -29,12 +34,8 @@ let currentAskedCount: number = 0;
 let currentTotalQuestion: number = 0;
 
 // Generation New Quiz
-const generateNewQuizBtn = document.querySelector<HTMLButtonElement>("#getDataButton");
-if (generateNewQuizBtn) {
-    generateNewQuizBtn.addEventListener("click", getData);
-} else {
-    console.error("Button with ID 'generateNewQuizBtn' not found");
-}
+const generateNewQuizBtn = document.getElementById("getDataButton") as HTMLButtonElement;
+generateNewQuizBtn.addEventListener("click", getData);
 
 // Provided an event listener attached to the DOMContentLoaded event
 document.addEventListener("DOMContentLoaded", () => {
@@ -97,8 +98,8 @@ function hideDownloadResultButton() {
 // Quiz parameters display on screen
 
 function setQuizParameters() {
-    const selectDifficultyElement = document.querySelector<HTMLOptionElement>("#selected_difficulty");
-    const selectedCategoryElement = document.querySelector<HTMLOptionElement>("#selected_category");
+    const selectDifficultyElement = document.getElementById("selected_difficulty") as OptionsElement;
+    const selectedCategoryElement = document.getElementById("selected_category") as OptionsElement;
 
     if (selectDifficultyElement) {
         selectDifficulty = selectDifficultyElement.value;
@@ -173,7 +174,7 @@ function setupQuizWithFunFacts() {
 
             // const storedFunData = funData;
 
-            const nextQuestionElelemnt = document.querySelector<HTMLButtonElement>("#next-question");
+            const nextQuestionElelemnt = document.getElementById("next-question") as ButtonElement;
             nextQuestionElelemnt?.addEventListener("click", () => {
                 // displayRandomFunFact(storedFunData);
                 displayRandomFunFact(funData);
@@ -209,7 +210,7 @@ function eventListeners() {
 
 // FunFacts random display function
 function displayRandomFunFact(funData: FunData[]) {
-    const funFactElement = document.querySelector<HTMLInputElement>(".fun-fatcs-p");
+    const funFactElement = document.querySelector(".fun-fatcs-p") as InputElement;
     const randomFunFact = getRandomFunFact(funData);
     if (funFactElement) {
         funFactElement.textContent = `"${randomFunFact}"`;
